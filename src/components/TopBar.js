0272936622c8f6d56762fc71371bf22cb33c8d92
RichardsGee <richardsgee68@gmail.com>
2024-10-01 22:02:26 -0300

    Mudanças visuais

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -1,36 +1,44 @@
-// src/components/TopBar.js
 import React from 'react';
+import styles from '../styles/topbar.module.css'; // Certifique-se de que o caminho do CSS Module está correto
 
 function TopBar() {
   return (
-    <header className="top-bar">
-      <div className="top-bar-item">
-        <div className="top-bar-content">
+    <header className={styles.topBar}>
+      <div className={styles.topBarItem}>
+        <div className={styles.topBarContent}>
           <h3>Faturamento</h3>
           <p id="faturamento-value">R$ 50,000</p>
         </div>
-        <div className="top-bar-icon"><span className="material-icons">attach_money</span></div>
+        <div className={styles.topBarIcon}>
+          <span className={`material-icons ${styles.icon}`}>attach_money</span>
+        </div>
       </div>
-      <div className="top-bar-item">
-        <div className="top-bar-content">
+      <div className={styles.topBarItem}>
+        <div className={styles.topBarContent}>
           <h3>Vendas</h3>
           <p id="vendas-value">120</p>
         </div>
-        <div className="top-bar-icon"><span className="material-icons">shopping_cart</span></div>
+        <div className={styles.topBarIcon}>
+          <span className={`material-icons ${styles.icon}`}>shopping_cart</span>
+        </div>
       </div>
-      <div className="top-bar-item">
-        <div className="top-bar-content">
+      <div className={styles.topBarItem}>
+        <div className={styles.topBarContent}>
+          <h3>Custo</h3>
+          <p id="custo-value">R$ 20,000</p>
+        </div>
+        <div className={styles.topBarIcon}>
+          <span className={`material-icons ${styles.icon}`}>money_off</span>
+        </div>
+      </div>
+      <div className={styles.topBarItem}>
+        <div className={styles.topBarContent}>
           <h3>Lucro</h3>
           <p id="lucro-value">R$ 15,000</p>
         </div>
-        <div className="top-bar-icon"><span className="material-icons">trending_up</span></div>
-      </div>
-      <div className="top-bar-item">
-        <div className="top-bar-content">
-          <h3>Publicidade</h3>
-          <p id="publicidade-value">R$ 5,000</p>
+        <div className={styles.topBarIcon}>
+          <span className={`material-icons ${styles.icon}`}>trending_up</span>
         </div>
-        <div className="top-bar-icon"><span className="material-icons">campaign</span></div>
       </div>
     </header>
   );
