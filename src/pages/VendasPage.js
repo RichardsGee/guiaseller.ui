f05b0673bc23446161a5f8814ef0c30ee025f743
RichardsGee <richardsgee68@gmail.com>
2024-10-02 03:06:55 -0300

    update

--- src/pages/VendasPage.js
+++ src/pages/VendasPage.js
@@ -1,47 +1,99 @@
-// src/pages/VendasPage.js
-import React, { useState, useEffect } from 'react';
-import DashboardLayout from '../layouts/DashboardLayout';
+import React, { useState, useEffect, useContext } from 'react';
+import Header from '../components/Header';
+import Sidebar from '../components/Sidebar';
+import TopBar from '../components/TopBar';
+import Footer from '../components/Footer';
+import { AuthContext } from '../context/AuthContext';
 import styles from '../styles/vendas.module.css'; // Importando o CSS Module
 
 function VendasPage() {
   const [vendas, setVendas] = useState([]);
 
+  // Utilizando o contexto de autenticação para pegar informações do usuário
+  const { user, signOut } = useContext(AuthContext);
+  const username = user ? user.displayName || user.email : "No User Logged";
+  const userPhoto = user ? user.photoURL : null; 
+  const userEmail = user ? user.email : null;
+
   useEffect(() => {
     const vendasData = [
-      { id: 1, produto: 'Produto A', cliente: 'Cliente 1', valor: 'R$ 100,00', data: '01/10/2024' },
-      { id: 2, produto: 'Produto B', cliente: 'Cliente 2', valor: 'R$ 200,00', data: '02/10/2024' },
+      {
+        id: 9811,
+        imagem: 'img_url_1', // URL da imagem do produto
+        sku: 'JARR027',
+        marketplaceEnvio: 'Kwai / Kwai',
+        nome: 'Gislene Machado',
+        venda: 'R$ 109,90',
+        custo: 'R$ 10,99',
+        imposto: 'R$ 8,79',
+        lucro: 'R$ 90,12',
+        margem: '82,00%',
+        status: 'Aguardando Pagamento',
+      },
+      {
+        id: 9810,
+        imagem: 'img_url_2',
+        sku: 'CO691486',
+        marketplaceEnvio: 'Magalu / Coleta',
+        nome: 'Rosany Vescovi',
+        venda: 'R$ 29,93',
+        custo: 'R$ 16,99',
+        imposto: 'R$ 2,39',
+        lucro: 'R$ 10,55',
+        margem: '35,29%',
+        status: 'Aguardando Pagamento',
+      },
+      // Adicione mais vendas conforme necessário
     ];
     setVendas(vendasData);
   }, []);
 
   return (
-    <DashboardLayout>
-      <div className={styles.vendasContainer}>
-        <h1 className={styles.vendasTitle}>Lista de Vendas</h1>
-        <table className={styles.vendasTable}>
-          <thead>
-            <tr>
-              <th>ID</th>
-              <th>Produto</th>
-              <th>Cliente</th>
-              <th>Valor</th>
-              <th>Data</th>
-            </tr>
-          </thead>
-          <tbody>
-            {vendas.map((venda) => (
-              <tr key={venda.id}>
-                <td>{venda.id}</td>
-                <td>{venda.produto}</td>
-                <td>{venda.cliente}</td>
-                <td>{venda.valor}</td>
-                <td>{venda.data}</td>
+    <div className="container">
+      <Header username={username} logout={signOut} />
+      <Sidebar userPhoto={userPhoto} username={username} userEmail={userEmail} />
+      <div className="main-content">
+        <TopBar userPhoto={userPhoto} />
+        <div className={styles.vendasContainer}>
+          <h1 className={styles.vendasTitle}>Meus Pedidos</h1>
+          <table className={styles.vendasTable}>
+            <thead>
+              <tr>
+                <th>Número</th>
+                <th>Imagem</th>
+                <th>SKU</th>
+                <th>Marketplace/Envio</th>
+                <th>Nome</th>
+                <th>Venda</th>
+                <th>Custo</th>
+                <th>Imposto</th>
+                <th>Lucro</th>
+                <th>Margem</th>
+                <th>Status</th>
               </tr>
-            ))}
-          </tbody>
-        </table>
+            </thead>
+            <tbody>
+              {vendas.map((venda) => (
+                <tr key={venda.id}>
+                  <td>{venda.id}</td>
+                  <td><img src={venda.imagem} alt="Produto" className={styles.vendaImage}/></td>
+                  <td>{venda.sku}</td>
+                  <td>{venda.marketplaceEnvio}</td>
+                  <td>{venda.nome}</td>
+                  <td>{venda.venda}</td>
+                  <td>{venda.custo}</td>
+                  <td>{venda.imposto}</td>
+                  <td>{venda.lucro}</td>
+                  <td>{venda.margem}</td>
+                  <td>{venda.status}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
       </div>
-    </DashboardLayout>
+      <Footer />
+    </div>
   );
 }
 
