263e18780b2c5167c2c94b2cb358db37e5c0d0f9
Jonas Tomaz <jonas.aquinos@hotmail.com>
2024-10-26 00:55:00 -0300

    feat: axios Callback

--- src/pages/Callback/Callback.jsx
+++ src/pages/Callback/Callback.jsx
@@ -1,21 +1,60 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+import axios from 'axios';
 
 const Callback = () => {
+  const getCodeParams = () => {
+    const url = new URL(window.location.href);
+    const params = new URLSearchParams(url.search);
+    const code = params.get('code');
+    return code;
+  };
 
+  const handleIntegration = async () => {
+    const access_token = "fakeAccessToken123"; 
+    const user_marketplace_id = "marketplaceUser123";
+    const userId = "X02jGDhoFoao7SfG6yiQMquN8cB2"; 
+    const authorization_code = getCodeParams();
+    const nickname = "SellerNickname";
+    const power_seller_status = "gold";
+    const level_id = "level1";
+    const permalink = "https://marketplace.com/seller123";
+    const total = 1500;
 
-    function getCodeParams() {
-        const url = new URL(window.location.href);
-        const params = new URLSearchParams(url.search);
-        const code = params.get('code');
-        return code;
+    const requestData = {
+      access_token,
+      user_marketplace_id,
+      userId,
+      authorization_code,
+      nickname,
+      power_seller_status,
+      level_id,
+      permalink,
+      total
+    };
+
+    try {
+      const response = await axios.post('/integrations', requestData, {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+
+      console.log('Integração criada com sucesso:', response.data);
+    } catch (error) {
+      console.error('Erro ao criar integração:', error);
     }
+  };
+
+  useEffect(() => {
+    handleIntegration(); 
+  }, []);
 
   return (
     <div>
-        <h1>Hello, i'm callback</h1>
-        <button onClick={() => console.log(getCodeParams())}>Pegue esse code no log ai Richardão</button>
+      <h1>Hello, I'm callback</h1>
+      <button onClick={() => console.log(getCodeParams())}>Pegue esse code no log ai Richardão</button>
     </div>
-  )
-}
+  );
+};
 
-export default Callback
+export default Callback;
