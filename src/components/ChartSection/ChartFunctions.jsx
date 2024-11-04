7861f974f641b893dc49de7f111ec427baf750e3
RichardsGee <richardsgee68@gmail.com>
2024-11-04 03:01:22 -0300

    update

--- src/components/ChartSection/ChartFunctions.jsx
+++ src/components/ChartSection/ChartFunctions.jsx
@@ -1,3 +1,6 @@
+import { startOfToday, endOfYesterday } from 'date-fns';
+
+// Resumo de vendas e faturamento para um intervalo de dias
 export const calculateSummary = (salesData, days) => {
   const today = new Date();
   const startDate = new Date();
@@ -17,6 +20,7 @@ export const calculateSummary = (salesData, days) => {
   return { totalSales, totalRevenue };
 };
 
+// Resumo do mês atual
 export const calculateCurrentMonthSummary = (salesData) => {
   const today = new Date();
   const startDate = new Date(today.getFullYear(), today.getMonth(), 1); // Primeiro dia do mês atual
@@ -35,6 +39,7 @@ export const calculateCurrentMonthSummary = (salesData) => {
   return { totalSales, totalRevenue };
 };
 
+// Resumo do mês anterior
 export const calculatePreviousMonthSummary = (salesData) => {
   const today = new Date();
   const startDate = new Date(today.getFullYear(), today.getMonth() - 1, 1); // Primeiro dia do mês anterior
@@ -54,18 +59,17 @@ export const calculatePreviousMonthSummary = (salesData) => {
   return { totalSales, totalRevenue };
 };
 
-// Função para calcular o total de vendas e faturamento de hoje
+// Total de vendas e faturamento de hoje
 export const calculateTodaySummary = (salesData) => {
-  const today = new Date();
-  const todayStart = new Date(today.getFullYear(), today.getMonth(), today.getDate());
-  const todayEnd = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 1); // Para garantir que pega até o fim do dia
+  const today = startOfToday(); // 00:00:00 de hoje
+  const todayEnd = new Date(today.getTime() + (24 * 60 * 60 * 1000)); // 00:00:00 de amanhã
 
   let totalSales = 0;
   let totalRevenue = 0;
 
   salesData.forEach(sale => {
     const dateCreated = new Date(sale.date_created);
-    if (dateCreated >= todayStart && dateCreated < todayEnd) {
+    if (dateCreated >= today && dateCreated < todayEnd) {
       totalSales += 1;
       totalRevenue += sale.total_amount || 0;
     }
@@ -74,12 +78,12 @@ export const calculateTodaySummary = (salesData) => {
   return { totalSales, totalRevenue };
 };
 
-// Função para calcular o total de vendas e faturamento de ontem
+// Total de vendas e faturamento de ontem
 export const calculateYesterdaySummary = (salesData) => {
-  const yesterday = new Date();
-  yesterday.setDate(yesterday.getDate() - 1);
+  const yesterday = endOfYesterday(); // 23:59:59 do dia anterior
+
   const yesterdayStart = new Date(yesterday.getFullYear(), yesterday.getMonth(), yesterday.getDate());
-  const yesterdayEnd = new Date(yesterday.getFullYear(), yesterday.getMonth(), yesterday.getDate() + 1);
+  const yesterdayEnd = new Date(yesterday.getFullYear(), yesterday.getMonth(), yesterday.getDate() + 1); // 00:00:00 do dia atual
 
   let totalSales = 0;
   let totalRevenue = 0;
@@ -95,7 +99,7 @@ export const calculateYesterdaySummary = (salesData) => {
   return { totalSales, totalRevenue };
 };
 
-// Função que retorna todos os resumos, incluindo hoje e ontem
+// Retorna todos os resumos, incluindo hoje e ontem
 export const calculateAllSummaries = (salesData) => {
   const todaySummary = calculateTodaySummary(salesData);
   const yesterdaySummary = calculateYesterdaySummary(salesData);
