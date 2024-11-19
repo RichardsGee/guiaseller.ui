d4c4ba983ba985ee8e943e650b4e3b13802821d0
Jonas Tomaz <jonas.aquinos@hotmail.com>
2024-11-19 01:10:16 -0300

    add logout functionality to AlphaScreen component

--- src/components/AlphaScreen/AlphaScreen.jsx
+++ src/components/AlphaScreen/AlphaScreen.jsx
@@ -1,7 +1,18 @@
 import React from 'react';
 import styles from './AlphaScreen.module.css';
+import { AuthContext } from '../../context/AuthContext';
+import { useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const AlphaAccess = () => {
+  const { user, signOut } = useContext(AuthContext);
+  const navigate = useNavigate();
+  
+  const logout = () => {
+    signOut();
+    navigate('/');
+  }
+
   return (
     <div className={styles.alphaAccessContainer}>
       <div className={styles.alphaAccessContent}>
@@ -14,6 +25,7 @@ const AlphaAccess = () => {
             Desculpe, ainda estamos em acesso alpha. Se você já garantiu o seu acesso e está vendo esta tela, entre em contato conosco.
           </p>
           <button className={styles.contactButton}>Entrar em Contato</button> {/* Botão adicionado */}
+          <button className={styles.logoutButton} onClick={logout}>Logout</button>
         </div>
       </div>
       <div className={styles.topBanner}>Guia Seller - Alpha Version</div> {/* Banner agora na parte de baixo */}
